4441f741840f850939c7336b27dc364c30d476ba
minwook09 <minwook0008@gmail.com>
2023-05-21 21:30:27 +0900

    audio 저장

--- djangoProject/beta/models.py
+++ djangoProject/beta/models.py
@@ -9,3 +9,12 @@ class Singer(models.Model):
     song1 = models.CharField(max_length=50, null=False)
     song2 = models.CharField(max_length=50, null=False)
     song3 = models.CharField(max_length=50, null=False)
+
+
+class Audio(models.Model):
+    file = models.FileField(upload_to='audio')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.file.name
+
